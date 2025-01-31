e832eb1bb50dfdfc41cf864bfb3fd4074e78c703
MaksWyszatycki <apaczek320@gmail.com>
2025-01-31 23:19:39 +0100

    123

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [currentStation, setCurrentStation] = useState(stations[0])
   const [isPlaying,setIsPlaying]=useState(false)
   const [currentTime, setCurrentTime] = useState(new Date());
-  const audioRef = useRef(new Audio(currentStation.url))
+  const audioRef = useRef(null)
   const [location, setLocation] = useState(null);
 const [volume, setVolume] = useState(0.5);
 
@@ -23,6 +23,7 @@ const [volume, setVolume] = useState(0.5);
     const timer = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
+    audioRef.current=new Audio(currentStation.url)
     return () => clearInterval(timer);
   }, []);
 
@@ -47,7 +48,7 @@ const [volume, setVolume] = useState(0.5);
 
 
   const handleVolumeChange = (event) => {
-    const newVolume = parseFloat(event.target.value);
+    const newVolume = parseFloat(event.target.value) / 100;
     setVolume(newVolume);
     if (audioRef.current) {
       audioRef.current.volume = newVolume;
@@ -71,6 +72,7 @@ const [volume, setVolume] = useState(0.5);
           <button onClick={togglePlay}>
             {isPlaying ? "Pauza" : "Odtwórz"}
           </button>
+          <input type="range" min="0" max="100"  onChange={handleVolumeChange} />
           <div className="station-list">
             {stations.map((station, index) => (
               <button
